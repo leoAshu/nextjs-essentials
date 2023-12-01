88a4b31c2adc78cd7a022cec3bb431b2ca7aaec0
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-01 14:29:25 -0800

    uses daisyui to define a tabular view of the users

--- project/app/users/page.tsx
+++ project/app/users/page.tsx
@@ -12,7 +12,7 @@ const UsersPage = async () => {
   return (
     <>
     <h1>Users</h1>
-    <p>{new Date().toLocaleTimeString()}</p>
+    {/* <p>{new Date().toLocaleTimeString()}</p> */}
     <table className="table table-bordered">
       <thead>
         <tr>
